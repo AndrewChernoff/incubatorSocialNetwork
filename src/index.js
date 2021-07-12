ff09bf246d9de7b0eb457be9722a1edbf54924a2
Andrew <whitedrew538@gmail.com>
2021-07-12 20:54:52 +0500

    renamed state.js to store.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
-
+////////////
 let renderEntireTree = (state) => {
   ReactDOM.render(
       <React.StrictMode>
