95f33f7ef4597c86fdca34103f9782ee59d6dbd0
Andrew <whitedrew538@gmail.com>
2021-07-12 14:47:12 +0500

    changed if statements to switch case in Reducer files

--- src/redux/dialogReducer.js
+++ src/redux/dialogReducer.js
@@ -2,18 +2,20 @@ const Send_Message = 'Send-Message';
 const Update_Message_Body = 'Update-Message-Body';
 
 const dialogReducer = (state, action) => {
-    if (action.type === Send_Message) {
-        let newMessage = {
-            message: state.newMessageText,
-            id: '6'
-        }
-        state.message.push(newMessage);
-        state.newMessageText = '';
+    switch (action.type) {
+        case Send_Message:
+            let newMessage = {
+                message: state.newMessageText,
+                id: '6'
+            }
+            state.message.push(newMessage);
+            state.newMessageText = '';
+            return state;
+        case Update_Message_Body:
+            state.newMessageText = action.newText;
+            return state;
+            default: return state;
     }
-    else if (action.type === Update_Message_Body) {
-        state.newMessageText = action.newText;
-    }
-    return state
 }
 
 export default dialogReducer;
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -2,19 +2,22 @@ const Add_Post = 'Add-Post';
 const Update_Post_Text = 'Update-Post-Text';
 
 const profileReducer = (state, action) => {
-    if (action.type === Add_Post) {
-        let newPost = {
-            id: 3,
-            message: state.newPostText,
-            likeCount: '0'
-        }
-        state.posts.push(newPost);
-        state.newPostText = '';
+    switch (action.type) {
+        case Add_Post:
+            let newPost = {
+                id: 3,
+                message: state.newPostText,
+                likeCount: '0'
+            }
+            state.posts.push(newPost);
+            state.newPostText = '';
+            return state;
+
+            case Update_Post_Text:
+                state.newPostText = action.newText;
+                return state;
+                default: return state;
     }
-    else if (action.type === Update_Post_Text) {
-        state.newPostText = action.newText;
-    }
-    return state
 }
 
 export default profileReducer
