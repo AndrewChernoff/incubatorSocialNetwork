fbf1df4490fbcf9dcec1289fa8c69cab388f431f
Andrew <whitedrew538@gmail.com>
2021-08-04 18:17:50 +0500

    made first ajax request

--- src/components/FindUsers/Users.jsx
+++ src/components/FindUsers/Users.jsx
@@ -1,53 +1,21 @@
+import * as axios from "axios";
 import React from "react";
 import s from './Users.module.css';
+import userPhoto from './../../assets/images/user.png';
 
 const Users = (props) => {
-debugger
+
    if (props.users.length === 0) { 
-   props.setUsers ( [
-        {
-            id: 1,
-            photoURL: 'https://yt3.ggpht.com/ytc/AAUvwnihEIew1x--ivyCNeGwa13y-FShBYm9rGl_sXxVEg=s800-c-k-c0x00ffffff-no-rj',
-            followed: false,
-            fullName: 'Andrew Chernoff',
-            status: 'Everything is awesome',
-            location: { country: 'USA', city: 'Chicago' }
-        },
-
-        {
-            id: 2,
-            photoURL: 'https://yt3.ggpht.com/ytc/AAUvwnihEIew1x--ivyCNeGwa13y-FShBYm9rGl_sXxVEg=s800-c-k-c0x00ffffff-no-rj',
-            followed: true,
-            fullName: 'Artemus Dolgin',
-            status: 'Chudila from Zhytomir',
-            location: { country: 'USA', city: 'Los Andgeles' }
-        },
-
-        {
-            id: 3,
-            photoURL: 'https://yt3.ggpht.com/ytc/AAUvwnihEIew1x--ivyCNeGwa13y-FShBYm9rGl_sXxVEg=s800-c-k-c0x00ffffff-no-rj',
-            followed: true,
-            fullName: 'Serega Rastorguev',
-            status: 'That was it',
-            location: { country: 'USA', city: 'Los Andgeles' }
-        },
-
-        {
-            id: 4,
-            photoURL: 'https://yt3.ggpht.com/ytc/AAUvwnihEIew1x--ivyCNeGwa13y-FShBYm9rGl_sXxVEg=s800-c-k-c0x00ffffff-no-rj',
-            followed: false,
-            fullName: 'Alisher Abdulloev',
-            status: 'Assalam Aleikum, pharmasists',
-            location: { country: 'Germany', city: 'Berlin' }
-        }
-    ]
-    )
+       axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+           debugger
+        props.setUsers (response.data.items);
+       })
 }
     return (
         <div className={s.findUsersPage}>
             {props.users.map(u => <div key={u.id}>
                 <span>
-                    <div> <img src={u.photoURL} className={s.userPhoto} /></div>
+                    <div> <img src={u.photos.small != null? u.photos.small : userPhoto} className={s.userPhoto} /></div>
                     <div>
                         {u.followed
                             ? <button onClick={() => { props.unfollow(u.id) }}>unfollow</button>
@@ -57,12 +25,12 @@ debugger
 
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
             </div>
