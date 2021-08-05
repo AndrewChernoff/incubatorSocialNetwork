abe3881d4a15ae08ce584f419a26bd302ff651e5
Andrew <whitedrew538@gmail.com>
2021-08-05 15:23:18 +0500

    changed functional component Users to class component

--- src/components/FindUsers/Users.jsx
+++ src/components/FindUsers/Users.jsx
@@ -3,19 +3,69 @@ import React from "react";
 import s from './Users.module.css';
 import userPhoto from './../../assets/images/user.png';
 
-const Users = (props) => {
+class Users extends React.Component {
 
-   if (props.users.length === 0) { 
-       axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-           debugger
-        props.setUsers (response.data.items);
-       })
+    constructor(props) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items);
+        })
+    }
+
+    render() {
+        return (
+            <div className={s.findUsersPage}>
+                {this.props.users.map(u => <div key={u.id}>
+                    <span>
+                        <div> <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} /></div>
+                        <div>
+                            {u.followed
+                                ? <button onClick={() => { this.props.unfollow(u.id) }}>unfollow</button>
+                                : <button onClick={() => { this.props.follow(u.id) }}>follow</button>}
+                        </div>
+                    </span>
+
+                    <span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
+                        </span>
+                    </span>
+                </div>
+                )}
+            </div>
+        )
+    }
 }
+
+export default Users;
+
+/* import * as axios from "axios";
+import React from "react";
+import s from './Users.module.css';
+import userPhoto from './../../assets/images/user.png';
+
+const Users = (props) => {
+    let getUSers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                debugger
+                props.setUsers(response.data.items);
+            })
+        }
+    }
+
     return (
         <div className={s.findUsersPage}>
+            <button onClick={getUSers}>Get users</button>
+
             {props.users.map(u => <div key={u.id}>
                 <span>
-                    <div> <img src={u.photos.small != null? u.photos.small : userPhoto} className={s.userPhoto} /></div>
+                    <div> <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} /></div>
                     <div>
                         {u.followed
                             ? <button onClick={() => { props.unfollow(u.id) }}>unfollow</button>
@@ -39,4 +89,4 @@ const Users = (props) => {
     )
 }
 
-export default Users;
+export default Users; */
