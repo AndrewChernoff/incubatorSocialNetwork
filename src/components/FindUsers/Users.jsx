288766720cbd3ed1ff8c732108cff8d4a9fdd9a9
Andrew <whitedrew538@gmail.com>
2021-08-05 16:08:27 +0500

    added first life cycle method

--- src/components/FindUsers/Users.jsx
+++ src/components/FindUsers/Users.jsx
@@ -4,42 +4,41 @@ import s from './Users.module.css';
 import userPhoto from './../../assets/images/user.png';
 
 class Users extends React.Component {
-
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            this.props.setUsers(response.data.items);
-        })
-    }
-
-    render() {
-        return (
-            <div className={s.findUsersPage}>
-                {this.props.users.map(u => <div key={u.id}>
-                    <span>
-                        <div> <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} /></div>
-                        <div>
-                            {u.followed
-                                ? <button onClick={() => { this.props.unfollow(u.id) }}>unfollow</button>
-                                : <button onClick={() => { this.props.follow(u.id) }}>follow</button>}
-                        </div>
-                    </span>
-
-                    <span>
-                        <span>
-                            <div>{u.name}</div>
-                            <div>{u.status}</div>
-                        </span>
-                        <span>
-                            <div>{'u.location.country'}</div>
-                            <div>{'u.location.city'}</div>
-                        </span>
-                    </span>
-                </div>
-                )}
-            </div>
+            this.props.setUsers(response.data.items)
+        }
         )
-    }
+}
+
+render() {
+    return (
+        <div className={s.findUsersPage}>
+            {this.props.users.map(u => <div key={u.id}>
+                <span>
+                    <div> <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} /></div>
+                    <div>
+                        {u.followed
+                            ? <button onClick={() => { this.props.unfollow(u.id) }}>unfollow</button>
+                            : <button onClick={() => { this.props.follow(u.id) }}>follow</button>}
+                    </div>
+                </span>
+
+                <span>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
+                    </span>
+                </span>
+            </div>
+            )}
+        </div>
+    )
+}
 }
 
 export default Users;
